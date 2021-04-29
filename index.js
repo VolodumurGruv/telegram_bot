062718d81f082c778a60df8129bf8353d4d2429b
VolodumurGruv <gru.v@i.ua>
2021-04-30 01:54:39 +0300

    a bit change the order

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const botApi = process.env.HEROKU_BOT;
 
 const bot = new TelegramBot(botApi, { polling: true });
 
-bot.on("message", (msg) => {
-	bot.sendMessage(msg.chat.id, `Hello ${msg.from.first_name}`);
-});
-
 bot.onText(/\/echo(.+)/, (msg, match) => {
 	const chatId = msg.chat.id;
 	const resp = match[1];
 
 	bot.sendMessage(chatId, resp);
+});
+
+bot.on("message", (msg) => {
+	bot.sendMessage(msg.chat.id, `Hello ${msg.from.first_name}`);
 });
