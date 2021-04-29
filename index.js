b9da3db8ae50614caac46751a2a5437dc3e55f3a
VolodumurGruv <gru.v@i.ua>
2021-04-30 02:12:29 +0300

    comented onText

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const botApi = process.env.HEROKU_BOT;
 
 const bot = new TelegramBot(botApi, { polling: true });
 
-bot.onText(/\/echo(.+)/, (msg, match) => {
-	const chatId = msg.chat.id;
-	const resp = match[1];
-	const time = Date().getTime();
-	notes.push({ time: time });
-	bot.sendMessage(chatId, resp);
-});
+// bot.onText(/\/echo(.+)/, (msg, match) => {
+// 	const chatId = msg.chat.id;
+// 	const resp = match[1];
+// 	const time = Date().getTime();
+// 	notes.push({ time: time });
+// 	bot.sendMessage(chatId, resp);
+// });
 
 bot.on("message", (msg) => {
 	bot.sendMessage(msg.chat.id, `Hello ${msg.from.first_name}`);
